f30582f5cfe7607e6810aa4f14fd67914ebc302e
Christos Dalamagkas <cdalamagkas@gmail.com>
2022-09-10 23:13:34 +0300

    correct timestamp in offline mode, fixed format for ping/pong

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ The script reads Open Charge Point Protocol (OCPP) packets and outputs their con
 There are two modes supported:
 - **ONLINE**: This mode continiously captures packets from an interface that should be manually specified. Each TCP packet will be passed through a Python Scapy pipeline engine, which will heuristically identify whether it has a WebSocket payload. If masked, the WebSocket payload will be unmasked, and then saved to a timestamped log file using the Python logging facility.
 - **OFFLINE**: Does the same procedure, but reads pcaps instead.
+
+Regardless of the mode, the output logs are generated in the `./output-logs` directory (relevant to the `logGenerator.py` location).
+
+Any input pcaps for the offline mode should be placed in the `./pcaps` directory (relevant to the `logGenerator.py` location).
--- logGenerator.py
+++ logGenerator.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-from datetime import datetime
+from datetime import date, datetime
 import logging
 from logging.handlers import RotatingFileHandler
 import json
@@ -10,13 +10,22 @@ ONLINE_MODE = False
 # PCAP_FILES is needed only if operating in offline mode (i.e., ONLINE_MODE = False)
 PCAP_FILES = ["RemoteStartTransaction.pcap"]
 
+#### LOGGING CONFIGURATION ###################
+class TimestampFilter(logging.Filter):
+    def filter(self, record):
+        if hasattr(record, 'timestamp'):
+            record.created = record.timestamp
+        return True
+
 LOG_FILENAME = datetime.now().strftime("%Y%m%d-%H%M%S") + "_ocppLogs.json"
-FORMAT = '%(asctime)s.%(msecs)03d %(message)s'
+FORMAT = '%(asctime)s.%(msecs)03dZ %(message)s'
 DATEFMT = '%Y-%m-%dT%H:%M:%S'
 formatter = logging.Formatter(fmt=FORMAT, datefmt=DATEFMT)
 logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt=DATEFMT)
 LOGGER = logging.getLogger("Rotating Log")
-handler = RotatingFileHandler(LOG_FILENAME, maxBytes=15728640, backupCount=5)
+filter = TimestampFilter()
+LOGGER.addFilter(filter)
+handler = RotatingFileHandler("./output-logs/" + LOG_FILENAME, maxBytes=15728640, backupCount=5)
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
 
@@ -29,9 +38,9 @@ def analysePacketOCPP(packet):
         return False
 
     if payload[0] == 138:  # corresponds to \x8a (pong)
-        return  {"src_ip": packet["IP"].src, "dst_ip": packet["IP"].dst,"msg": "pong"} 
+        return  json.dumps({"src_ip": packet["IP"].src, "dst_ip": packet["IP"].dst,"msg": "pong"})
     elif payload[0] == 137: # corresponds to \x89 (ping)
-        return  {"src_ip": packet["IP"].src, "dst_ip": packet["IP"].dst,"msg": "ping"}
+        return  json.dumps({"src_ip": packet["IP"].src, "dst_ip": packet["IP"].dst,"msg": "ping"})
     elif payload[0] == 129:  # 129 corresponds to \x81 (opcode=text), 137 to \x89 (ping), 138 to \x8a (pong)
         unmasked = []
         if payload[1] & 128 == 128:  # Check if mask bit is set
@@ -91,10 +100,11 @@ if __name__ == "__main__":
     else:
         for pcap in PCAP_FILES:
             packets = rdpcap("./pcaps/" + pcap)
+            BASE_TIMESTAMP = time.time()
             for packet in packets:
                 if packet.haslayer("TCP"):
                     result = analysePacketOCPP(packet)
                     if not result:
                         continue
                     else:
-                        LOGGER.info(result)
+                        LOGGER.info(msg=result, extra={'timestamp': packet.time})
